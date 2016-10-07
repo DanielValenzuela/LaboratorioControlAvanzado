8d38f8b717e24fd60ed05f2acfd81277994a264f
Gustavo Díaz <g.hernan.diaz@gmail.com>
2016-10-07 20:11:00 -0300

    agrego nuevos datos

--- experiencia1/actividad1.m
+++ experiencia1/actividad1.m
@@ -1,8 +1,8 @@
 %%Parametros del modelo fenomenologico, segun regresion lineal
 %Datos experimento 1. Valvula de salida cerrada
 
-% Fin = [36 97 151 152 215]';
-% frecuencias = (30:17.5:100)';
+Fin = [36 97 151 152 215]';
+frecuencias = (30:17.5:100)';
 
 % getParametros
 % Fin = FlujosProm';
