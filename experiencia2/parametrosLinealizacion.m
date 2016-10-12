e2b22aa7eaa72a2b9ed4235d6720eb136e4b612e
Gustavo Díaz <g.hernan.diaz@gmail.com>
2016-10-12 04:07:11 -0300

    modelo no lineal del pendulo invertido

--- experiencia2/parametrosLinealizacion.m
+++ experiencia2/parametrosLinealizacion.m
@@ -7,18 +7,19 @@ l = 0.3213;	%[m]
 b = 0.05;	%[Ns/m]
 I = 0.0138;	%[Kg m^2]
 
-
+b_prima = b+0.548;
+% F_prima=1.88*u-4.7;
 
 dPhi3_dx2 = g*[((M+m)*(I+m*l^2))/((M+m)*(I+m*l^2)-(m^2)*(l^2))-1];
-dPhi3_dx3 = [(-b*(I+m*l^2))/((M+m)*(I+m*(l^2))-(m^2)*(l^2))];
+dPhi3_dx3 = [(-b_prima*(I+m*l^2))/((M+m)*(I+m*(l^2))-(m^2)*(l^2))];
 dPhi3_dx4 = 0;
-dPhi4_dx2 = [((M+m)*g)/((M+m)*(I+m*(l^2))-(m^2)*(l^2))];
-dPhi4_dx3 = [(m*l*b)/((M+m)*(I+m*(l^2))-(m^2)*(l^2))];
+dPhi4_dx2 = [((M+m)*m*g*l)/((M+m)*(I+m*(l^2))-(m^2)*(l^2))];
+dPhi4_dx3 = [(-m*l*b_prima)/((M+m)*(I+m*(l^2))-(m^2)*(l^2))];
 dPhi4_dx4 = 0;
 
 
-dPhi3_dF = (I+m*(l^2))/((M+m)*(I+m*(l^2))-(m^2)*(l^2));
-dPhi4_dF = (m*l)/((M+m)*(I+m*(l^2))-(m^2)*(l^2));
+dPhi3_dF = 1.88*(I+m*(l^2))/((M+m)*(I+m*(l^2))-(m^2)*(l^2));
+dPhi4_dF = (1.88*m*l)/((M+m)*(I+m*(l^2))-(m^2)*(l^2));
 
 %Mostrar valores en consola
 % dPhi3_dx2
