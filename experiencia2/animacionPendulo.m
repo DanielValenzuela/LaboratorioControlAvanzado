a8f0760a0c27b00bb415247d027408dc180b6a66
Gustavo Díaz <g.hernan.diaz@gmail.com>
2016-10-15 19:23:38 -0300

    actualizo animacion pendulo

--- experiencia2/animacionPendulo.m
+++ experiencia2/animacionPendulo.m
@@ -1,7 +1,9 @@
 figure(1);
 for k=1:length(theta.time);
 	%hold on
-	plot([0,1*exp(j*(theta.signals.values(k)-(pi/2)))],'o-');
+	thetaAux = theta.signals.values(k)*pi/180;
+	arg = thetaAux-(pi/2);
+	plot([0,1*exp(j*arg)],'o-');
 	axis equal;
 	axis(1.2*[-1 1 -1 1]);
 	grid on;
