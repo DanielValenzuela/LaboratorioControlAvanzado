f09a8f3664e8485761528a7383283653909a5e8c
Gustavo Díaz <g.hernan.diaz@gmail.com>
2016-11-20 18:04:41 -0300

    Revert "update"
    This reverts commit 3943d656d184cb290606cc20675a76c0902c8663.

--- experiencia2/graphInTime.m
+++ experiencia2/graphInTime.m
@@ -3,16 +3,12 @@ function [ ] = graphInTime( t_vector, t_signal, name , limits, units, newFigure,
 	%figure('Name',strcat('Señal', name),'NumberTitle','off');
 	if (newFigure==1)
 		% hold off
-		% disp('1');
 		figure('Name', name,'NumberTitle','off');
 	else
-		% disp('0');
 		hold on
 	end
 	plot(t_vector,t_signal, c);        % Grafico de la señal 
-	xlim(limits);
-	if (newFigure==1)
+	xlim(limits)
 	title(strcat('Grafico de ',name,' en el Tiempo'), 'interpreter', 'latex')
-	end
 	xlabel('Tiempo [s]'); ylabel(strcat('Amplitud [ ', units, ' ]'));
 end
